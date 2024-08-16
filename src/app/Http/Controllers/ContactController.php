f60bf6b5dee1ae54389811007f8b9fadaf818a58
myoksnt <santa.miyaoka@gmail.com>
2024-08-16 18:04:06 +0900

    ability

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -11,37 +11,37 @@ use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class ContactController extends Controller
 {
-  public function index()
-  {
-    $categories = Category::all();
-    return view('contact', compact('categories'));
-  }
+    public function index()
+    {
+        $categories = Category::all();
+        return view('contact', compact('categories'));
+    }
 
-  public function confirm(ContactRequest $request)
-  {
-    $contacts = $request->all();
-    $category = Category::find($request->category_id);
-    return view('confirm', compact('contacts', 'category'));
-  }
+    public function confirm(ContactRequest $request)
+    {
+        $contacts = $request->all();
+        $category = Category::find($request->category_id);
+        return view('confirm', compact('contacts', 'category'));
+    }
 
-  public function store(ContactRequest $request)
-  {
-    if ($request->has('back')) {
-    return redirect('/')->withInput();
-  }
+    public function store(ContactRequest $request)
+    {
+        if ($request->has('back')) {
+            return redirect('/')->withInput();
+        }
 
-    $request['tell'] = $request->tel_1 . $request->tel_2 . $request->tel_3;
-    Contact::create(
-      $request->only([
-          'category_id',
-          'first_name',
-          'last_name',
-          'gender',
-          'email',
-          'tell',
-          'address',
-          'building',
-          'detail'
+        $request['tell'] = $request->tel_1 . $request->tel_2 . $request->tel_3;
+        Contact::create(
+            $request->only([
+                'category_id',
+                'first_name',
+                'last_name',
+                'gender',
+                'email',
+                'tell',
+                'address',
+                'building',
+                'detail'
             ])
         );
 
@@ -50,16 +50,16 @@ class ContactController extends Controller
 
     public function admin()
     {
-      $contacts = Contact::with('category')->paginate(7);
-      $categories = Category::all();
-      $csvData = Contact::all();
-      return view('admin', compact('contacts', 'categories', 'csvData'));
+        $contacts = Contact::with('category')->paginate(7);
+        $categories = Category::all();
+        $csvData = Contact::all();
+        return view('admin', compact('contacts', 'categories', 'csvData'));
     }
 
     public function search(Request $request)
     {
-      if ($request->has('reset')) {
-          return redirect('/admin')->withInput();
+        if ($request->has('reset')) {
+            return redirect('/admin')->withInput();
         }
         $query = Contact::query();
 
@@ -69,43 +69,43 @@ class ContactController extends Controller
         $csvData = $query->get();
         $categories = Category::all();
         return view('admin', compact('contacts', 'categories', 'csvData'));
-    }   
-    
+    }
+
     public function destroy(Request $request)
     {
-      Contact::find($request->id)->delete();
-      return redirect('/admin');
+        Contact::find($request->id)->delete();
+        return redirect('/admin');
     }
 
     public function export(Request $request)
     {
-      $query = Contact::query();
+        $query = Contact::query();
 
-      $query = $this->getSearchQuery($request, $query);
+        $query = $this->getSearchQuery($request, $query);
 
-      $csvData = $query->get()->toArray();
+        $csvData = $query->get()->toArray();
 
-      $csvHeader = [
-          'id', 'category_id', 'first_name', 'last_name', 'gender', 'email', 'tell', 'address', 'building', 'detail', 'created_at', 'updated_at'
+        $csvHeader = [
+            'id', 'category_id', 'first_name', 'last_name', 'gender', 'email', 'tell', 'address', 'building', 'detail', 'created_at', 'updated_at'
         ];
 
         $response = new StreamedResponse(function () use ($csvHeader, $csvData) {
-          $createCsvFile = fopen('php://output', 'w');
+            $createCsvFile = fopen('php://output', 'w');
 
-          mb_convert_variables('SJIS-win', 'UTF-8', $csvHeader);
+            mb_convert_variables('SJIS-win', 'UTF-8', $csvHeader);
 
-          fputcsv($createCsvFile, $csvHeader);
+            fputcsv($createCsvFile, $csvHeader);
 
-          foreach ($csvData as $csv) {
-              $csv['created_at'] = Date::make($csv['created_at'])->setTimezone('Asia/Tokyo')->format('Y/m/d H:i:s');
-              $csv['updated_at'] = Date::make($csv['updated_at'])->setTimezone('Asia/Tokyo')->format('Y/m/d H:i:s');
+            foreach ($csvData as $csv) {
+                $csv['created_at'] = Date::make($csv['created_at'])->setTimezone('Asia/Tokyo')->format('Y/m/d H:i:s');
+                $csv['updated_at'] = Date::make($csv['updated_at'])->setTimezone('Asia/Tokyo')->format('Y/m/d H:i:s');
                 fputcsv($createCsvFile, $csv);
             }
 
-          fclose($createCsvFile);
+            fclose($createCsvFile);
         }, 200, [
-          'Content-Type' => 'text/csv',
-          'Content-Disposition' => 'attachment; filename="contacts.csv"',
+            'Content-Type' => 'text/csv',
+            'Content-Disposition' => 'attachment; filename="contacts.csv"',
         ]);
 
         return $response;
@@ -113,11 +113,11 @@ class ContactController extends Controller
 
     private function getSearchQuery($request, $query)
     {
-      if(!empty($request->keyword)) {
-          $query->where(function ($q) use ($request) {
-            $q->where('first_name', 'like', '%' . $request->keyword . '%')
-                  ->orWhere('last_name', 'like', '%' . $request->keyword . '%')
-                  ->orWhere('email', 'like', '%' . $request->keyword . '%');
+        if(!empty($request->keyword)) {
+            $query->where(function ($q) use ($request) {
+                $q->where('first_name', 'like', '%' . $request->keyword . '%')
+                    ->orWhere('last_name', 'like', '%' . $request->keyword . '%')
+                    ->orWhere('email', 'like', '%' . $request->keyword . '%');
             });
         }
 
--- src/app/Models/Contact.php
+++ src/app/Models/Contact.php
@@ -5,16 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Contact extends Model
 {
     use HasFactory;
 
     protected $guarded = [
-        'id' ,
+        'id',
     ];
 
     public function category()
     {
         return $this->belongsTo(Category::class);
-    }    
+    }
 }
