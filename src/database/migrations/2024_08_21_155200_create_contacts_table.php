7cd574054b8009284506dd24f36e96945a1beb8e
myoksnt <santa.miyaoka@gmail.com>
2024-08-19 21:53:14 +0900

    ability

--- src/database/migrations/2024_08_21_155200_create_contacts_table.php
+++ src/database/migrations/2024_08_21_155200_create_contacts_table.php
@@ -16,8 +16,8 @@ class CreateContactsTable extends Migration
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('category_id')->constrained()->cascadeOnDelete();
-            $table->string('first-name');
-            $table->string('last-name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->tinyInteger('gender');
             $table->string('email');
             $table->string('tell');
